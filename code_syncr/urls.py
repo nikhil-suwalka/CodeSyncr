1e1b2c1688ffbe08f61b00b90cec54b76e6067a9
Nishit Shanbhag <nishit.shanbhag@gmail.com>
2020-09-07 19:02:05 +0530

    added home view

--- code_syncr/urls.py
+++ code_syncr/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from main_app.views import *
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',home_view,name="home_view")
 ]
